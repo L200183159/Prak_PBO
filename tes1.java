4a83f584c67c41b83f786c90901cc8c3a8ea0354
Naura Salsabila <44856077+L200183159@users.noreply.github.com>
2019-10-09 20:59:52 +0700

    Modul5

---
+++ tes1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas;
+
+/**
+ *
+ * @author ASUS
+ */
+public class tes1 {
+    public static void main(String[] args) {
+        Tugas tugas1 = new Tugas();
+        Tugas tugas2 = new Tugas("Naura Salsabila", "3 Agustus 2000");
+    }
+
+    
+}
